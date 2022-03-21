278ce7ea0d06f6ce1441cf2560ab6dfb323fec87
Anirach Mingkhwan <Anirach.m@fitm.kmutnb.ac.th>
2022-03-21 22:23:01 +0700

    to server

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ db.once('open',() => console.log('Connected to Database'))
 
 app.use(express.json())
 
-const subscribersRouter = require('./routes/subscribers')
+const subscribersRouter = require('./SVersions/subscribers')
 app.use('/subscribers', subscribersRouter)
 
 app.listen(3000, () => console.log('Server Started'))
