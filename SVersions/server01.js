3506e7ea0cacb97d37ea611322f0eb9d98382547
Anirach Mingkhwan <Anirach.m@fitm.kmutnb.ac.th>
2022-03-21 23:09:20 +0700

    change to server.js

--- SVersions/server01.js
+++ SVersions/server01.js
@@ -4,4 +4,8 @@ const mongoose = require('mongoose')
 
 mongoose.connect('mongodb://localhost/subscribes')
 
+app.get("/", (req, res) => {
+    res.send("Hello Node.js REST Server");
+  });
+
 app.listen(3000, () => console.log('Server Started'))
--- SVersions/server03.js
+++ SVersions/server03.js
@@ -7,4 +7,8 @@ const db = mongoose.connection
 db.on('error', (error) => console.error(error))
 db.once('open',() => console.log('Connected to Database'))
 
+app.get("/", (req, res) => {
+    res.send("Hello Node.js REST Server");
+  });
+
 app.listen(3000, () => console.log('Server Started'))
